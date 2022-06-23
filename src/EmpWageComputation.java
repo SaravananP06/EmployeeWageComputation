ad9dbb595649b0188e8a79096142c8a50ddda6a0
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-06-23 22:56:27 +0530

    [Saravanan]: Added Employee Daily Wage Calculation

--- src/EmpWageComputation.java
+++ src/EmpWageComputation.java
@@ -5,8 +5,14 @@ public class EmpWageComputation {
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == Is_Full_Time){
             System.out.println("Employee is Present");
+            int Wage_Per_Hour = 20;
+            int Full_Day_Hour = 8;
+            int total_Salary = (Wage_Per_Hour * Full_Day_Hour);
+            System.out.println("Salary for day is " +total_Salary);
         }else{
             System.out.println("Employee is Absent");
+            int total_salary = 0;
+            System.out.println("total salary is " +total_salary);
         }
     }
 }
