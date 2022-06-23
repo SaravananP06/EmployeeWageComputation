ffa986fee9be2f23d4094091da44775cad87b0e6
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-06-23 22:38:43 +0530

    [Saravanan]: Added Employee Attendence Functionality

--- src/EmpWageComputation.java
+++ src/EmpWageComputation.java
@@ -1,5 +1,12 @@
 public class EmpWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
+        int Is_Full_Time = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == Is_Full_Time){
+            System.out.println("Employee is Present");
+        }else{
+            System.out.println("Employee is Absent");
+        }
     }
 }
