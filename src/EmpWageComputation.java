9a899251edcae9c4eb2d004c7d53f4f2e8b007c9
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-06-23 23:56:25 +0530

    [Saravanan]: Added Part time wage

--- src/EmpWageComputation.java
+++ src/EmpWageComputation.java
@@ -2,18 +2,23 @@ public class EmpWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
         int Is_Full_Time = 1;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        int Is_Part_Time = 2;
+        int Wage_Per_Hour = 20;
+        int EmpWage = 0;
+        int EmpHrs = 0;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
         if (empCheck == Is_Full_Time){
-            System.out.println("Employee is Present");
-            int Wage_Per_Hour = 20;
-            int Full_Day_Hour = 8;
-            int total_Salary = (Wage_Per_Hour * Full_Day_Hour);
-            System.out.println("Salary for day is " +total_Salary);
-        }else{
+            System.out.println("Employee is in Full Time");
+            EmpHrs = 8;
+        } else if (empCheck == Is_Part_Time) {
+            System.out.println("Employee is in Part Time");
+            EmpHrs = 4;
+        } else{
             System.out.println("Employee is Absent");
-            int total_salary = 0;
-            System.out.println("total salary is " +total_salary);
-
+            EmpHrs = 0;
+        }
+        EmpWage = (EmpHrs * Wage_Per_Hour);
+        System.out.println("Employee Wage: " +EmpWage);
         }
     }
 }
