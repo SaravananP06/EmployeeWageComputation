d942d2b39727afcfca584332752d306d83672fce
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-06-24 07:17:24 +0530

    [Saravanan]: Added Switch Case in Emp Wage Program

--- src/EmpWageComputation.java
+++ src/EmpWageComputation.java
@@ -1,24 +1,23 @@
 public class EmpWageComputation {
+    public static final int Is_Full_Time = 1;
+    public static final int Is_Part_Time = 2;
+    public static final int Wage_Per_Hour = 20;
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation");
-        int Is_Full_Time = 1;
-        int Is_Part_Time = 2;
-        int Wage_Per_Hour = 20;
+
         int EmpWage = 0;
         int EmpHrs = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == Is_Full_Time){
-            System.out.println("Employee is in Full Time");
-            EmpHrs = 8;
-        } else if (empCheck == Is_Part_Time) {
-            System.out.println("Employee is in Part Time");
-            EmpHrs = 4;
-        } else{
-            System.out.println("Employee is Absent");
-            EmpHrs = 0;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck){
+            case Is_Full_Time:
+                EmpHrs = 8;
+                break;
+            case Is_Part_Time:
+                EmpHrs = 4;
+                break;
+            default:
+                EmpHrs = 0;
         }
         EmpWage = (EmpHrs * Wage_Per_Hour);
         System.out.println("Employee Wage: " +EmpWage);
-        }
     }
 }
